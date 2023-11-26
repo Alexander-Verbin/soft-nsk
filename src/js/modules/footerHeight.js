46754664206b1748dce841576ceea6e02b1c138d
Alexander-Verbin <sash.verbin@yandex.ru>
2023-11-26 22:23:22 +0300

    site

--- src/js/modules/footerHeight.js
+++
@@ -1,9 +0,0 @@
-export const _footerHeight = () => {
-	const footer = document.querySelector(".footer");
-	const footerHeight = footer.getBoundingClientRect().height;
-
-	document.documentElement.style.setProperty(
-		"--footer-height",
-		`${footerHeight / 16}rem`
-	);
-};
--- src/js/modules/headerHeight.js
+++
@@ -1,9 +0,0 @@
-export const _headerHeight = () => {
-	const header = document.querySelector(".header");
-	const headerHeight = header.getBoundingClientRect().height;
-
-	document.documentElement.style.setProperty(
-		"--header-height",
-		`${headerHeight / 16}rem`
-	);
-};
